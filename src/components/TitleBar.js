a69fe22669cb6f4b1a796c18c7f76b45402abdb6
dabeen <bdb7979@naver.com>
2024-02-29 11:23:54 +0900

    TitleBarComponent

---
+++ src/components/TitleBar.js
@@ -0,0 +1,46 @@
+import {useEffect} from 'react';
+import {Platform, StatusBar, Dimensions} from 'react-native';
+import styled, {ThemeProvider} from 'styled-components/native';
+import {theme} from '../themes/theme';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+const screenHeight = Dimensions.get('window').height;
+const fontSize = screenHeight / 35;
+
+const TitleContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: ${({theme}) => theme.dayBackground};
+    padding-top: ${Platform.OS === 'android' ? StatusBar.currentHeight*1.2 : 0}px;
+    padding-left: ${StatusBar.currentHeight*0.4}px;
+    padding-right: ${StatusBar.currentHeight*0.4}px;
+`;
+const UserCircle = styled.Image`
+    width: ${fontSize*1.3}px;
+    height: ${fontSize*1.3}px;
+`;
+const Title = styled.Text`
+    font-size: ${fontSize}px;
+    fontFamily: GmarketSansTTFBold;
+    color: ${({theme}) => theme.dayText};
+`;
+
+const TitleBar = () => {
+    const [fontsLoaded] = useFonts({
+        "GmarketSansTTFBold": require("../../assets/fonts/GmarketSansTTFBold.ttf")
+    });
+
+    if(!fontsLoaded) {
+        return null;
+    };
+
+    return (
+        <TitleContainer theme={theme}>
+            <Title theme={theme}>Weather Wear</Title>
+            <UserCircle source={require('../../assets/icons/user.png')} />
+        </TitleContainer>
+    );
+}
+
+export default TitleBar;
--- src/screens/WeatherMain.js
+++ src/screens/WeatherMain.js
@@ -1,39 +1,19 @@
 import {useEffect} from 'react';
-import {Platform, StatusBar, Dimensions} from 'react-native';
 import styled, {ThemeProvider} from 'styled-components/native';
 import {theme} from '../themes/theme';
 import {useFonts} from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import TitleBar from '../components/TitleBar';
 
 SplashScreen.preventAutoHideAsync();
-const screenHeight = Dimensions.get('window').height;
-const fontSize = screenHeight / 35;
 const Container = styled.SafeAreaView`
     flex: 1;
     flex-direction: column;
     background-color: ${({theme}) => theme.dayBackground};
 `;
-const TitleBar = styled.SafeAreaView`
-    flex-direction: row;
-    justify-content: space-between;
-    background-color: ${({theme}) => theme.dayBackground};
-    padding-top: ${Platform.OS === 'android' ? StatusBar.currentHeight*1.2 : 0}px;
-    padding-left: ${StatusBar.currentHeight*0.4}px;
-    padding-right: ${StatusBar.currentHeight*0.4}px;
-`;
-const UserCircle = styled.Image`
-    width: ${fontSize*1.3}px;
-    height: ${fontSize*1.3}px;
-`;
-const Title = styled.Text`
-    font-size: ${fontSize}px;
-    fontFamily: GmarketSansTTFBold;
-    color: ${({theme}) => theme.dayText};
-`;
 
 const WeatherMain = () => {
     const [fontsLoaded] = useFonts({
-        "GmarketSansTTFBold": require("../../assets/fonts/GmarketSansTTFBold.ttf"),
         "GmarketSansTTFLight": require("../../assets/fonts/GmarketSansTTFLight.ttf"),
         "GmarketSansTTFMedium": require("../../assets/fonts/GmarketSansTTFMedium.ttf")
     });
@@ -50,15 +30,12 @@ const WeatherMain = () => {
 
     if(!fontsLoaded) {
         return null;
-    }
+    };
         
     return (
         <ThemeProvider theme={theme}>
             <Container>
-                <TitleBar>
-                    <Title>Weather Wear</Title>
-                    <UserCircle source={require('../../assets/icons/user.png')} />
-                </TitleBar>
+                <TitleBar theme={theme} />
             </Container>
         </ThemeProvider>
     );
